50b7498c6f57dd70f0e37258728b38d393667d98
Dawlz <37235288+Dawlz@users.noreply.github.com>
2021-08-01 20:57:42 +0100

    remove cached

--- README.md
+++
@@ -1,2 +0,0 @@
-My first practice app in MERN Stack
-I built this a long time ago, but finally decided to publish on github today :)
--- server/config/config.mongoose.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/newDb', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-})
-    .then(() => console.log("Connection Successfully established"))
-    .catch(()=> console.log("Connection to the database failed"));
